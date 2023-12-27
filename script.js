d5c7dd0fe322c56dac8fcb800af26a0e7a42eda3
Martin McNamara <martin@hkc.ie>
2023-12-27 22:40:42 +0000

    Switch from using P5.js lib to the canvas API

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 import { Star } from "./star.js";
 
 let starField = [];
+let ctx = {};
+let canvasEl = {};
 
 // Creates an array of star objects with random positions
 const starFieldRandom = function (qty) {
@@ -14,54 +16,56 @@ const starFieldRandom = function (qty) {
   return starField;
 };
 
-// Using P5 in instance mode
-const s = function (p) {
-  p.setup = function () {
-    p.createCanvas(window.innerWidth, window.innerHeight);
+// Initalise the canvas to full screen
+// Generate starField array of stars
+// Start the rendering
+const init = function (starCount) {
+  canvasEl = document.querySelector(".canvas-view");
+  ctx = canvasEl.getContext("2d", { alpha: false });
 
-    // Create Random start array
-    starField = starFieldRandom(500);
+  canvasEl.width = window.innerWidth;
+  canvasEl.height = window.innerHeight;
 
-    p.stroke(255);
-    p.textSize(32);
-
-    //frameRate();
-    //noLoop(); // Run once and stop
-  };
-
-  p.draw = function () {
-    p.background(0);
-
-    // Remove the off screen Stars
-    const countStars = starField.length;
-    const starFieldTemp = starField.filter((star) => star.startOnScreen());
-    starField = starFieldTemp;
-
-    // Replace stars removed off screen
-    const removedStarCount = countStars - starField.length;
-    if (removedStarCount > 0) {
-      //console.log(`Adding ${removedStarCount} removed Stars`);
-      for (let index = 0; index < removedStarCount; index++)
-        starField.push(new Star(true));
-    }
-
-    // Plot each Star
-    starField.forEach((el) => {
-      p.strokeWeight(el.starSize);
-      p.point(...el.starPosition);
-      el.starUpdate();
-    });
-  };
-
-  p.windowResized = function () {
-    p.resizeCanvas(window.innerWidth, window.innerHeight);
-  };
+  starField = starFieldRandom(starCount);
+  requestAnimationFrame(render);
 };
 
-new p5(s); // invoke p5
+// Render the starfield
+const render = function () {
+  // Remove the off screen Stars
+  const countStars = starField.length;
+  const starFieldTemp = starField.filter((star) => star.startOnScreen());
+  starField = starFieldTemp;
 
-// const updateSize = function () {
-//   resizeCanvas(window.innerWidth, window.innerHeight);
-// };
+  // Replace removed Stars with new ones
+  const removedStarCount = countStars - starField.length;
+  if (removedStarCount) {
+    for (let index = 0; index < removedStarCount; index++)
+      starField.push(new Star(true));
+  }
 
-// window.addEventListener("resize", updateSize);
+  // Clear Canvas
+  ctx.clearRect(0, 0, window.innerWidth, window.innerHeight);
+
+  // Plot each Star
+  starField.forEach((el) => {
+    // Draw the star
+    ctx.beginPath();
+    ctx.arc(...el.starPosition, el.starSize / 2, 0, 2 * Math.PI);
+    ctx.fillStyle = "white";
+    ctx.fill();
+
+    // update Star
+    el.starUpdate();
+  });
+
+  requestAnimationFrame(render);
+};
+
+// Handle Window resize
+window.addEventListener("resize", () => {
+  canvasEl.width = window.innerWidth;
+  canvasEl.height = window.innerHeight;
+});
+
+init(500);
